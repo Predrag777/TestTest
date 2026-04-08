9f9784c7ed1812474577b09c345b46e99bfd304b
Predrag777 <predragbrajovicSS@gmail.com>
2026-04-08 15:52:53 +0200

    New animations and swordsmenship

---
+++ Assets/Scripts/EnemyCombat.cs
@@ -0,0 +1,107 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyCombat : MonoBehaviour
+{
+    Animator animator;
+    public int health=3;
+    AudioSource source;
+    NavMeshAgent agent;
+    Transform player;
+
+    [SerializeField] AudioClip hitSound;
+    [SerializeField] AudioClip death;
+    [SerializeField] float keepDistance = 2f;
+    [SerializeField] float attackCooldown = 1.5f;
+
+    bool isAttacking = false;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        source=GetComponent<AudioSource>();
+        animator=GetComponent<Animator>();
+        agent=GetComponent<NavMeshAgent>();
+        
+        GameObject playerObj = GameObject.FindGameObjectWithTag("Player");
+        if(playerObj != null)
+            player = playerObj.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(player == null || health <= 0)
+        {
+            if(agent.enabled) agent.ResetPath();
+            animator.SetFloat("speed", 0f);
+            return;
+        }
+
+        animator.SetBool("swordActive", true);
+
+        float distance = Vector3.Distance(transform.position, player.position);
+
+        
+
+        if(distance > keepDistance)
+        {
+            agent.isStopped = false;
+            agent.SetDestination(player.position);
+            animator.SetFloat("speed", agent.velocity.magnitude);
+        }
+        else
+        {
+            agent.isStopped = true;
+            animator.SetFloat("speed", 0f);
+
+            // Napadaj kad si dovoljno blizu
+            if(!isAttacking)
+            {
+                isAttacking = true;
+                animator.SetTrigger("sword");
+                Invoke("ResetAttack", attackCooldown);
+            }
+        }
+
+        // Uvek gledaj ka igracu
+        Vector3 lookDir = player.position - transform.position;
+        lookDir.y = 0f;
+        if(lookDir.sqrMagnitude > 0.01f)
+        {
+            Quaternion rot = Quaternion.LookRotation(lookDir);
+            transform.rotation = Quaternion.Slerp(transform.rotation, rot, Time.deltaTime * 5f);
+        }
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.CompareTag("katana"))
+        {
+            
+            Debug.Log("Pogodjen je sa "+collider.tag);
+            if(health>0){
+                source.PlayOneShot(hitSound);
+                animator.SetTrigger("hit");
+            }else{
+                source.PlayOneShot(death);
+                animator.SetTrigger("death");
+            }
+        }
+    }
+
+    public void hurt()
+    {
+        health--;
+    }
+
+    void ResetAttack()
+    {
+        isAttacking = false;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Sudario sam se sa: " + collision.gameObject.name);
+    }
+}
--- Assets/Scripts/PlayerScripts/CombatController.cs
+++ Assets/Scripts/PlayerScripts/CombatController.cs
@@ -9,6 +9,12 @@ public class CombatController : MonoBehaviour
     public GameObject swordObj;
 
     int count=1;
+    AudioSource source;
+    [SerializeField] AudioClip swordDraw;
+    [SerializeField] AudioClip swordReturn;
+
+    [SerializeField] AudioClip swing1;
+    [SerializeField] AudioClip swing2;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -17,24 +23,34 @@ public class CombatController : MonoBehaviour
         animator=currMove.animator;
         myMask=GetComponentInChildren<MaskProp>();
 
+        source=GetComponent<AudioSource>();
         swordObj=myMask.weapons[1];
     }
-
+    int c=1;
     // Update is called once per frame
     void Update()
     {
         if(currMove.selectedWeapons!=1) return;
-        
+
+        currMove.animator.SetBool("swordActive", swordPull);
+
+        if (Input.GetKeyDown(KeyCode.J)) //Activate block
+        {
+            c++; 
+        }
+        currMove.animator.SetBool("block", (c%2==0));
+
         if (!swordPull && Input.GetMouseButtonDown(0))
         {
             //Debug.Log("SWORD ACTIVE "+swordPull);
             //swordObj.SetActive(true);
-            
+            source.PlayOneShot(swordDraw);
             currMove.animator.SetTrigger("pull");
         }
 
         if(swordPull && Input.GetMouseButtonDown(0))
         {
+            source.PlayOneShot(swing2);
             currMove.animator.SetTrigger(("slash"+count));
 
         }
@@ -44,6 +60,7 @@ public class CombatController : MonoBehaviour
 
         if(swordPull && Input.GetMouseButtonDown(1))
         {
+            source.PlayOneShot(swordReturn);
             currMove.animator.SetTrigger("return");
             swordPull=false;
         }
